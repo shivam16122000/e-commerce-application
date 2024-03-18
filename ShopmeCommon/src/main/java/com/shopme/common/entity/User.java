31704522ab01e41faf65a89d5ca2a606904e792f
Shivam Tiwari <shivamt161222000@gmail.com>
2024-03-18 18:25:10 +0530

    added pagination,sorting, and better responsinve UI

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -1,124 +1,146 @@
 package com.shopme.common.entity;
 
-import jakarta.persistence.*;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 128, nullable = false, unique = true)
+	private String email;
+	
+	@Column(length = 64, nullable = false)
+	private String password;
+	
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+	
+	@Column(length = 64)
+	private String photos;
+	
+	private boolean enabled;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+			)
+	private Set<Role> roles = new HashSet<>();
+	
+	public User() {
+	}
+	
+	public User(String email, String password, String firstName, String lastName) {
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 
-    @Column(length = 128, nullable = false, unique = true)
-    private String email;
 
-    @Column(length = 64, nullable = false)
-    private String password;
+	public Integer getId() {
+		return id;
+	}
 
-    @Column(name = "first_name", length = 45, nullable = false)
-    private String firstName;
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    @Column(name = "last_name", length = 45, nullable = false)
-    private String lastName;
+	public String getEmail() {
+		return email;
+	}
 
-    @Column(length = 64)
-    private String photos;
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    private boolean enabled;
+	public String getPassword() {
+		return password;
+	}
 
-    @ManyToMany
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles = new HashSet<>();
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public User() {
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public User(String email, String password, String firstName, String lastName) {
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public String getPhotos() {
+		return photos;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public boolean isEnabled() {
+		return enabled;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public Set<Role> getRoles() {
+		return roles;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhotos() {
-        return photos;
-    }
-
-    public void setPhotos(String photos) {
-        this.photos = photos;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public void addRole(Role role) {
-        this.roles.add(role);
-    }
-
-    @Override
-    public String toString() {
-        return "User [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
-                + ", roles=" + roles + "]";
-    }
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", roles=" + roles + "]";
+	}
+	
+	@Transient
+	public String getPhotosImagePath() {
+		if (id == null || photos == null) return "/images/default-user.png";
+		
+		return "/user-photos/" + this.id + "/" + this.photos;
+	}
+	
+	@Transient
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+	
 }
